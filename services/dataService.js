f374e0dd8978ec9a6e0cdf04e682c517dcd2c8f5
Andrewnazeh <andro.nazeh160194@ci.menofia.edu.eg>
2024-05-18 14:06:22 +0300

    solve bugs

--- services/dataService.js
+++ services/dataService.js
@@ -40,7 +40,7 @@ exports.addData = asyncHandler(async (req, res, next) => {
     }
 
     // Schedule the task to run every 24 hours
-    cron.schedule('0 0 * * *', () => {
+    cron.schedule('* * * * *', () => {
         console.log('Running update task...');
         resetVariables();
     }, {
